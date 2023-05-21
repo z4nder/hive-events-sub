91f44fe40f5f34d7546a6e6e6cd75a3794675c49
z4nder <gusttavo212@gmail.com>
2023-05-21 10:04:53 -0300

    Fix mail template

--- server/actions/sendgrid.js
+++ server/actions/sendgrid.js
@@ -2,7 +2,7 @@
 import sgMail from '@sendgrid/mail'
 
 const fromEmail = "contato@hivecommunity.dev"
-const tempalteId = "d-1dddc287f5cab2f4219a14f1f62cfcc81fb"
+const tempalteId = "d-1c287f5cab2f4219a14f1f62cfcc81fb"
 
 export const sendSubEmail = async (sendgridClient, email)  => {
     const msg = {
@@ -13,7 +13,7 @@ export const sendSubEmail = async (sendgridClient, email)  => {
     
     try {
         await sendgridClient.send(msg);
-        return {success: true, message: null}
+    return {success: true, message: null}
     } catch (error) {
         return {success: false, message: error}
     }
