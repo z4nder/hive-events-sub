31025b9edbb622dc49417fecb20ca1f71fcec1c0
z4nder <gusttavo212@gmail.com>
2023-05-23 11:37:06 -0300

    Add vercel analytcs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({  
-    css: ['~/assets/css/main.css'],
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
+  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-    app: { 
-        head: { 
-            link: [ 
-                {rel: 'icon', type: "image/x-icon", href: '/img/logo_render.svg'},
-                {
-                    rel: 'stylesheet',
-                    href: 'https://fonts.googleapis.com/css2?family=DM+Sans&family=Nunito:wght@200;400;600&display=swap'
-                }
-            ] 
-        } 
-    }
-})
+  },
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/img/logo_render.svg" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=DM+Sans&family=Nunito:wght@200;400;600&display=swap",
+        },
+      ],
+    },
+  },
+});
